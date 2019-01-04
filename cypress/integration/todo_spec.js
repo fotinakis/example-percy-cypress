a6168cc1c497abbd858317a132c72a73bf072000
Mike Fotinakis <mike@fotinakis.com>
2019-01-03 16:51:31 -0800

    Yet another snapshot

--- cypress/integration/todo_spec.js
+++ cypress/integration/todo_spec.js
@@ -21,6 +21,7 @@ describe('TodoMVC', function() {
     // Add a new todo item.
     cy.get('.new-todo').should('exist')
     cy.get('.new-todo').type('New fancy todo {enter}')
+    cy.percySnapshot();
 
     // We should have 1 todo item showing in the todo list and the footer.
     cy.get('.todo-list').children('li').should('have.length', 1)
